9f42cc11dd53345da03a95bd699d8c42b35dc158
Mohammed Alyousef <may642_2000@hotmail.com>
2021-07-14 23:16:32 -0400

    Update example.rs

--- examples/example.rs
+++ examples/example.rs
@@ -1,4 +1,4 @@
-use fltk::{app, enums::Event, prelude::*, window};
+use fltk::{app, prelude::*, window};
 
 fn main() {
     let _app = app::App::default();
@@ -10,13 +10,6 @@ fn main() {
         .center_of_parent();
     win.end();
     win.show();
-
-    // close the app when the main window is closed
-    win.set_callback(|_| {
-        if app::event() == Event::Close {
-            std::process::exit(0);
-        }
-    });
 
     let mut wv = fltk_webview::Webview::create(false, &mut wv_win);
     wv.navigate("https://google.com");
