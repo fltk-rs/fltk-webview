bdf23cc4e17174f24ffa820308b21748041573cf
Mohammed Alyousef <may642_2000@hotmail.com>
2021-08-14 19:33:01 +0300

    Update cocoa.m

--- src/cocoa.m
+++ src/cocoa.m
@@ -1,8 +1,22 @@
 #import <Cocoa/Cocoa.h>
+#import <objc/runtime.h>
+#include <assert.h>
+
+@implementation NSWindow (KeyWindow)
+
+- (BOOL)my_canBecomeKeyWindow {
+  return YES;
+}
+
+@end
 
 void make_delegate(NSWindow *child, NSWindow *parent) {
   [parent setDelegate:(id)child];
   [child orderWindow:NSWindowAbove relativeTo:[parent windowNumber]];
+  Method old_method = class_getInstanceMethod([child class], @selector(canBecomeKeyWindow));
+  Method new_method = class_getInstanceMethod([child class], @selector(my_canBecomeKeyWindow));
+  assert(new_method);
+  method_exchangeImplementations(old_method, new_method);
   [child setIgnoresMouseEvents:NO];
   [child makeKeyAndOrderFront:nil];
 }
